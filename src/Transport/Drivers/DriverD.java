0e29dfa197d135b1e048e41ecaacc5504bcc5c0d
roman <roman.pshenichnikov555@gmail.com>
2022-11-02 16:50:14 +0300

    Course2_1.4add done

--- src/Transport/Drivers/DriverD.java
+++ src/Transport/Drivers/DriverD.java
@@ -1,62 +1,13 @@
 package Transport.Drivers;
 
-public class DriverD {
-    private String fio;
-    private String experience;
-    private String driversLicense;
+public class DriverD extends Driver {
 
     public DriverD(String fio, String experience, String driversLicense) {
-        this.fio = validOrDefaultFio(fio);
-        this.experience = validOrDefaultExperience(experience);
-        this.driversLicense = validOrDefaultDriversLicense(driversLicense);
+        super(fio, experience, driversLicense);
     }
 
-    public String getFio() {
-        return fio;
-    }
-
-    public void setFio(String fio) {
-        this.fio = fio;
-    }
-
-    public String getExperience() {
-        return experience;
-    }
-
-    public void setExperience(String experience) {
-        this.experience = experience;
-    }
-
-    public String getDriversLicense() {
-        return driversLicense;
-    }
-
-    public void setDriversLicense(String driversLicense) {
-        this.driversLicense = driversLicense;
-    }
-    private static String validOrDefaultFio(String fio) {
-        if (fio == null || fio.isEmpty() || fio.isBlank()) {
-            return  "Имя водителя не укаано";
-        } else {
-            return fio;
-        }
-    }
-    private static String validOrDefaultDriversLicense(String driversLicense) {
-        if (driversLicense == null || driversLicense.isEmpty() || driversLicense.isBlank()) {
-            return  "Нет прав";
-        } else {
-            return driversLicense;
-        }
-    }
-    private static String validOrDefaultExperience(String experience) {
-        if (experience == null || experience.isEmpty() || experience.isBlank()) {
-            return  "Нет стажа вождения";
-        } else {
-            return experience;
-        }
-    }
     @Override
-    public String toString() {
-        return "Водитель: " + fio + '\'' + " стаж: " + experience + '\'' + " лет, наличие водительских прав: " + driversLicense;
+    public void driveCar() {
+        System.out.println("Водитель: " + getFio() + " стаж: " + getExperience() +  " лет, наличие водительских прав: " + getDriversLicense());
     }
 }
